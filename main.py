66dddd771f71a0da38535e9a4c0d66efb9a974a7
peleg <lauferpeleg@gmail.com>
2025-11-29 14:05:36 +0200

    added: sqlite3 db, db setup, more organized functions and setup

--- main.py
+++ main.py
@@ -1,7 +1,20 @@
 import argparse
+import os.path
+import sqlite3
 
 class Todo:
+    def setup(self):
+        """
+        sets up: parsers, db_file
+        :return:
+        """
+        self.parsers_setup()
+        self.sqlite_setup()
+
     def parsers_setup(self):
+        """"
+        sets up parsers
+        """
         self.parser = argparse.ArgumentParser(prog="ToDo list",
                                          description="Add to a list and see all your Todo stuff",
                                          epilog="good luck and best regards"
@@ -15,17 +28,64 @@ class Todo:
                             help="add input to Todo List")
         self.parser_list = self.subparsers.add_parser('list',
                                             help='show todo list')
-        args = self.parser.parse_args()
 
+
+    def sqlite_setup(self):
+        """
+        checks if there is a db file self.db_file_path and if valid, creates one if not
+        """
+        self.db_file_path = r"listdb.db"
+        if not os.path.exists(self.db_file_path):
+            self.create_new_db_file()
+        elif not self.is_db_file_valid():
+            os.remove(self.db_file_path)
+            self.create_new_db_file()
+
+    def is_db_file_valid(self):
+        """
+
+       :return: True if the db file is good, false if not
+        """
+        # TODO: check if headers of file good
+        pass
+        return True
+
+    def create_new_db_file(self):
+        """
+        creates db file and tables
+        :return:
+        """
+        conn = sqlite3.connect(self.db_file_path)
+        cur = conn.cursor()
+        cur.execute("""
+                    CREATE TABLE list (
+                            todo_item TEXT NOT NULL
+                    );
+                    """)
+
+    def handle_req(self):
+        """
+        handles clients request
+        """
+        args = self.parser.parse_args()
         command = args.command
-        #print(args.subparser_name)
         if command == "list":
             print("printing todo list...")
+            # TODO: add printing list func
+            pass
         elif command == "add":
             print(f"adding new item to list: ")
+            print(args.item)
+            # TODO: add args.item to db file
+            pass
+        elif command == None:
+            print("printing help")
+            # TODO: print help stuff of parser
+            pass
 
     def __init__(self):
-        self.parsers_setup()
+        self.setup()
+        self.handle_req()
 
 if __name__ == '__main__':
     Todo()
